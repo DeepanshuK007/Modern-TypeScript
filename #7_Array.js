32b91583dbe9b4efe337e570e20452b3c6e848f7
DEEPANSHU KUMAR <97106571+DeepanshuK007@users.noreply.github.com>
2024-03-30 02:13:37 +0530

    Array

---
+++ #7_Array.js
@@ -0,0 +1,12 @@
+"use strict";
+//? In TS, an array is a data type that can store multiple values of diff data types sequentially.
+//TI(type inference) and TA(type annotation)
+//! Using square brackets:
+const numbs = [1, 5, 7, 9, 43]; //? type is array of number
+//! Using the Array constructor:
+const nums1 = new Array(1, 5, 7, 2, 8);
+//! Using the Array.of method
+//const names = Array.of("Deep", "Kalki", "D_Kumar")
+//todo Accessing elements using index:
+console.log(numbs[2]);
+//console.log(Array.of("Deep", "Kalki", "D_Kumar")) 
