7cbd190df979da998067f8acf1bdf1b64a05363e
DEEPANSHU KUMAR <97106571+DeepanshuK007@users.noreply.github.com>
2024-04-02 00:57:55 +0530

    Array_operations

---
+++ #8_ArrayOperations.ts
@@ -0,0 +1,40 @@
+const fruits: string[] = ["apple", "banana", "orange", "mango"]
+
+//todo Adding elements to an array using push:
+const updatedFruits = fruits.push("kiwi"); //? Adds at the end
+
+//! unshift inserts at the start
+const insertAtStart = fruits.unshift("Dragon_Fruit")  //donot shift
+console.log(updatedFruits); //? returns us the position at which pushed not the value nor the index
+console.log(fruits)
+
+//todo Removing elements from an array using pop:
+let lastElementRemovedFromArray = fruits.pop();  
+
+//! shift removes from the start
+//* pop returns the last element removed but shift does not.
+let removeFromStart = fruits.shift()
+console.log(lastElementRemovedFromArray);
+
+//! Iterating over elements:
+//? You can iterate over the elements of an array using various looping constructors such as for, for...of, or array methods like forEach.
+
+//* for loop
+for (let i=0; i<fruits.length; i++) {
+    console.log(fruits[i])  //* gives the index values as output
+}
+
+//* for...of/...in loop
+//? for in loop (iterates over indices) and for of (iterates over value)
+for(const fruit of fruits) {  //* returns the values, here we declare a var for the individual element in the arr first
+    console.log(fruit);
+}
+
+for(const fruit in fruits) {  //* returns the index
+    console.log (fruit);
+}
+
+//* forEach
+fruits.forEach((fruit:string)=>{  //! forEach is a method(arrow func) of array where in we pass fruit as a parameter 
+    console.log(fruit)
+})
